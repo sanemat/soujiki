61c094a85a71c977ac89dc965e3b7f853428f49f
sanemat <o.gata.ken@gmail.com>
2011-01-11 00:31:49 +0900

    add default editor

--- lib/soujiki.rb
+++ lib/soujiki.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 unless ENV['TUMBLR_URL']
   require 'pit'
+  ENV["EDITOR"] ||= "vim"
   config = Pit.get('tumblr', :require => {
     "url" => "your tumblr email:your tumblr password"
   })
